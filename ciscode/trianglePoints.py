dc7af62b405a63c8a3e59ce7111031ea484cff7f
suryanshshukla10 <suryanshshukla@outlook.com>
2021-12-14 10:28:15 -0500

    adding closest point on triangle file

---
+++ ciscode/trianglePoints.py
@@ -0,0 +1,236 @@
+from typing import Tuple
+import numpy as np
+from numpy import dot
+from math import sqrt
+
+
+def TriclosestPoint(TRI, P):
+    """[Calculate the distance of a given point P from a triangle TRI.]
+
+    Args:
+        TRI ([[P1;P2;P3]]): [The triangle is a matrix
+#   formed by three rows of points TRI = [P1;P2;P3] each of size 1x3]
+        P ([P0 = [0.5 -0.3 0.5]]): [Point P is a row vector of the form 1x3]
+    """
+    #
+    #        ^t
+    #  \     |
+    #   \reg2|
+    #    \   |
+    #     \  |
+    #      \ |
+    #       \|
+    #        *P2
+    #        |\
+    #        | \
+    #  reg3  |  \ reg1
+    #        |   \
+    #        |reg0\
+    #        |     \
+    #        |      \ P1
+    # -------*-------*------->s
+    #        |P0      \
+    #  reg4  | reg5    \ reg6
+    B = TRI[0, :]
+    E0 = TRI[1, :] - B
+    # E0 = E0/sqrt(sum(E0.^2)); %normalize vector
+    E1 = TRI[2, :] - B
+    # E1 = E1/sqrt(sum(E1.^2)); %normalize vector
+    D = B - P
+    a = dot(E0, E0)
+    b = dot(E0, E1)
+    c = dot(E1, E1)
+    d = dot(E0, D)
+    e = dot(E1, D)
+    f = dot(D, D)
+    # print "{0} {1} {2} ".format(B,E1,E0)
+    det = a * c - b * b
+    s = b * e - c * d
+    t = b * d - a * e
+
+    if (s + t) <= det:
+        if s < 0.0:
+            if t < 0.0:
+                # region4
+                if d < 0:
+                    # logging.info("region4")
+                    t = 0.0
+                    if -d >= a:
+                        s = 1.0
+                        sqrdistance = a + 2.0 * d + f
+                    else:
+                        s = -d / a
+                        sqrdistance = d * s + f
+                else:
+                    s = 0.0
+                    if e >= 0.0:
+                        t = 0.0
+                        sqrdistance = f
+                    else:
+                        if -e >= c:
+                            t = 1.0
+                            sqrdistance = c + 2.0 * e + f
+                        else:
+                            t = -e / c
+                            sqrdistance = e * t + f
+
+                            # of region 4
+            else:
+                # region 3
+                # logging.info("Point in region 3")
+                s = 0
+                if e >= 0:
+                    t = 0
+                    sqrdistance = f
+                else:
+                    if -e >= c:
+                        t = 1
+                        sqrdistance = c + 2.0 * e + f
+                    else:
+                        t = -e / c
+                        sqrdistance = e * t + f
+                        # of region 3
+        else:
+            if t < 0:
+                # region 5
+                # logging.info("Point in region 5")
+                t = 0
+                if d >= 0:
+                    s = 0
+                    sqrdistance = f
+                else:
+                    if -d >= a:
+                        s = 1
+                        sqrdistance = a + 2.0 * d + f  # GF 20101013 fixed typo d*s ->2*d
+                    else:
+                        s = -d / a
+                        sqrdistance = d * s + f
+            else:
+                # region 0
+                # logging.info("Point in region 0")
+                invDet = 1.0 / det
+                s = s * invDet
+                t = t * invDet
+                sqrdistance = s * (a * s + b * t + 2.0 * d) + \
+                    t * (b * s + c * t + 2.0 * e) + f
+    else:
+        if s < 0.0:
+            # region 2
+            # logging.info("Point in region 2")
+            tmp0 = b + d
+            tmp1 = c + e
+            if tmp1 > tmp0:  # minimum on edge s+t=1
+                numer = tmp1 - tmp0
+                denom = a - 2.0 * b + c
+                if numer >= denom:
+                    s = 1.0
+                    t = 0.0
+                    sqrdistance = a + 2.0 * d + f  # GF 20101014 fixed typo 2*b -> 2*d
+                else:
+                    s = numer / denom
+                    t = 1 - s
+                    sqrdistance = s * (a * s + b * t + 2 * d) + \
+                        t * (b * s + c * t + 2 * e) + f
+
+            else:  # minimum on edge s=0
+                s = 0.0
+                if tmp1 <= 0.0:
+                    t = 1
+                    sqrdistance = c + 2.0 * e + f
+                else:
+                    if e >= 0.0:
+                        t = 0.0
+                        sqrdistance = f
+                    else:
+                        t = -e / c
+                        sqrdistance = e * t + f
+                        # of region 2
+        else:
+            if t < 0.0:
+                # region6
+                # logging.info("region 6")
+                tmp0 = b + e
+                tmp1 = a + d
+                if tmp1 > tmp0:
+                    numer = tmp1 - tmp0
+                    denom = a - 2.0 * b + c
+                    if numer >= denom:
+                        t = 1.0
+                        s = 0
+                        sqrdistance = c + 2.0 * e + f
+                    else:
+                        t = numer / denom
+                        s = 1 - t
+                        sqrdistance = s * \
+                            (a * s + b * t + 2.0 * d) + t * \
+                            (b * s + c * t + 2.0 * e) + f
+
+                else:
+                    t = 0.0
+                    if tmp1 <= 0.0:
+                        s = 1
+                        sqrdistance = a + 2.0 * d + f
+                    else:
+                        if d >= 0.0:
+                            s = 0.0
+                            sqrdistance = f
+                        else:
+                            s = -d / a
+                            sqrdistance = d * s + f
+            else:
+                # region 1
+
+                numer = c + e - b - d
+                if numer <= 0:
+                    s = 0.0
+                    t = 1.0
+                    sqrdistance = c + 2.0 * e + f
+                else:
+                    denom = a - 2.0 * b + c
+                    if numer >= denom:
+                        s = 1.0
+                        t = 0.0
+                        sqrdistance = a + 2.0 * d + f
+                    else:
+                        s = numer / denom
+                        t = 1 - s
+                        sqrdistance = s * \
+                            (a * s + b * t + 2.0 * d) + t * \
+                            (b * s + c * t + 2.0 * e) + f
+
+    # account for numerical round-off error
+    if sqrdistance < 0:
+        sqrdistance = 0
+
+    dist = sqrt(sqrdistance)
+
+    PP0 = B + s * E0 + t * E1
+    # logging.info("distance:")
+    # logging.info(dist)
+    # logging.info("point coordinates")
+    # logging.info(PP0)
+
+    return dist, PP0
+
+
+def closestPoint(mS, mU, mT, sk):
+    """[it calcualtes the closest point on the triangle with finding the closest posssible point and matching the closest point on the triangle.]
+
+    Args:
+        mS ([np array 3135x3]): [vertex S of triangle]
+        mU ([np array 3135x3ype]): [vertex U of triangle]
+        mT ([np array 3135x3]): [vertex T of triangle]
+        sk ([np array nx3]): [input closest point value, ]
+
+    Returns:
+       ck [np array nx3]: [closest point on triangle to sk]
+    """
+
+    TRI = np.array([mS[0], mU[0], mT[0]])
+
+    dist, PP0 = TriclosestPoint(TRI, sk)
+    ck = PP0
+
+    print(TRI)
+
+    return ck
--- pa5.py
+++ pa5.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import numpy as np
 from scipy.spatial.distance import cdist
 
-from ciscode import readers, writers, registration
+from ciscode import readers, writers, registration, trianglePoints
 
 
 FORMAT = "%(message)s"
@@ -40,8 +40,9 @@ def main(
 
     #########triangle vetices##############################
     vertices = Vertices.arrVer
-    logging.info(vertices[1])
-    # logging.info(type(vertices))
+
+    ###########triangle Indices##########################
+    Indices = readers.Indices(data_dir / f"Problem5MeshFile.sur")
 
     #########calculation of FA,k and FB,k###########################
 
@@ -75,8 +76,6 @@ def main(
         frA = sampleReading.NA_dict  # rigid body A
         frB = sampleReading.NB_dict  # rigid body B
 
-        logging.info(frB[149])
-
         A = []
         dk = []
         for k in range(150):
@@ -97,11 +96,49 @@ def main(
     A = np.array(A)
     dk = np.array(dk)
 
-    # logging.info(A)
-    # logging.info(dk)
-    # logging.info(100*np.linalg.norm(A-dk))
+    # inital guess of Freg = I
+    sk = dk  # define sk
 
-    # print(np.shape(A))
+    def FindClosestPointMesh(sk):
+        """[It calculates the closest point in triangle mesh]
+
+        Args:
+            sk ([3x1 vector]): [description]
+
+        Returns:
+            [3x1]: [closest point]
+        """
+        triVert = Vertices.arrVer  # triangle vertices numpy array
+        triInd = Indices.arrInd  # triangle indices numpy array
+
+        # logging.info()
+        mS = []  # Coorrdinate Tri Vert U
+        mT = []  # Coorrdinate Tri Vert T
+        mU = []  # Coorrdinate Tri Vert U
+        for i in range(3135):
+            ks = triInd[i, 0]
+            ku = triInd[i, 1]
+            kt = triInd[i, 2]
+
+            ks = int(ks)  # indice "s" of Tri vert
+            ku = int(ku)  # indice "u" of Tri vert
+            kt = int(kt)  # indice "t" of Tri vert
+
+            S = triVert[ks-1]
+            U = triVert[ku-1]
+            T = triVert[kt-1]
+
+            mS.append(S)
+            mU.append(U)
+            mT.append(T)
+
+        mS = np.array(mS)  # coordinates of vertex S of Triangle
+        mU = np.array(mU)  # coordinates of vertex U of Triangle
+        mT = np.array(mT)  # coordinates of vertex T of Triangle
+
+        ck = trianglePoints.closestPoint(mS, mU, mT, sk)
+
+    c_k = FindClosestPointMesh(sk)
 
 
 if __name__ == "__main__":
